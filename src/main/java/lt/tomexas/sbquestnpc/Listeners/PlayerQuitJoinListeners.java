cc94dc80fd763c816f90cd8c925acabeeb40dc5a
ToMeXaS <tomex495@gmail.com>
2022-01-06 13:40:46 +0200

    NPCs not appearing
    Added a fix in onJoinEvent where player's that joined later than the said island owner, weren't receiving currently existing NPC packets.

--- src/main/java/lt/tomexas/sbquestnpc/Listeners/PlayerQuitJoinListeners.java
+++ src/main/java/lt/tomexas/sbquestnpc/Listeners/PlayerQuitJoinListeners.java
@@ -5,12 +5,20 @@ import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import lt.tomexas.sbquestnpc.PacketsEvents.PacketReader;
 import lt.tomexas.sbquestnpc.Skyblock;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityHeadRotation;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata;
+import net.minecraft.network.protocol.game.PacketPlayOutSpawnEntity;
+import net.minecraft.server.network.PlayerConnection;
+import net.minecraft.world.entity.npc.EntityVillager;
 import org.bukkit.*;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Map;
 
 public class PlayerQuitJoinListeners implements Listener {
 
@@ -33,6 +41,18 @@ public class PlayerQuitJoinListeners implements Listener {
 
             PacketReader reader = new PacketReader();
             reader.inject(player);
+
+            // Send already created entity packets to joined player
+            for (Map.Entry<Island, Map<EntityVillager, Location>> mapEntry : this.plugin.NPCs.entrySet()) {
+                if (mapEntry.getKey().equals(island)) continue;
+                Map<EntityVillager, Location> map = this.plugin.NPCs.get(mapEntry.getKey());
+                for (EntityVillager npc : map.keySet()){
+                    PlayerConnection connection = ((CraftPlayer) player).getHandle().b;
+                    connection.sendPacket(new PacketPlayOutSpawnEntity(npc));
+                    connection.sendPacket(new PacketPlayOutEntityMetadata(npc.getId(), npc.getDataWatcher(), true));
+                    connection.sendPacket(new PacketPlayOutEntityHeadRotation(npc, (byte) (npc.getBukkitYaw() * 256 / 360)));
+                }
+            }
         }
     }
 
